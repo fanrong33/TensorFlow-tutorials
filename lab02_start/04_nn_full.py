acf52a57eb4f0e159b436fd1745a826ebbbef9ee
fanrong33 <fanrong33@qq.com>
2017-05-10 23:33:14 +0800

    Update

--- lab02_start/04_nn_full.py
+++ lab02_start/04_nn_full.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 # 训练神经网络解决二分类问题
+# 
 # 总结训练神经网络的过程：
 # 1、定义神经网络的结构和前向传播的输出结果
 # 2、定义损失函数以及选择反向传播优化的算法
@@ -22,19 +23,28 @@ y_ = tf.placeholder(tf.float32, shape=(None, 1), name='y-input')
 a = tf.matmul(x, w1)
 y = tf.matmul(a, w2)
 
-# 定义损失函数的反向传播的算法
+# 定义损失函数的反向传播的算法 交叉熵
 cross_entropy = -tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
 train = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
 
 # 通过随机数生成一个模拟数据集
 rdm = RandomState(1)
 dataset_size = 128
-
 X = rdm.rand(dataset_size, 2)  #  X为shape=(128, 2)，128个数据集
+print(X)
+'''
+[[  4.17022005e-01   7.20324493e-01]
+ ...
+ [  1.14374817e-04   3.02332573e-01]]
+'''
 # 定义规则来给出样本的标签。在这里所有x1+x2<1的样例都被认为是正样本（比如零件合格），
 # 而其他为负样本（比如零件不合格）。和TensorFlow游乐场中的表示法不一样的地方是，
 # 在这里使用0来表示负样本，1来表示正样本。大部分解决分类问题的神经网络都会采用0和1的表示方法。
 Y = [[int(x1+x2 < 1)] for (x1, x2) in X]
+print(Y)
+'''
+[[0], ... , [1]]
+'''
 
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
@@ -53,17 +63,17 @@ with tf.Session() as sess:
 
     # 设定训练的轮数
     STEPS = 5000
-    for i in range(STEPS):
+    for step in range(STEPS):
         # 每次选取batch_size个样本进行训练。
-        start = (i * batch_size) % dataset_size
-        end = min(start+batch_size, dataset_size)
+        start = (step * batch_size) % dataset_size
+        end   = min(start+batch_size, dataset_size)
 
         # 通过选取的样本训练神经网络病更新参数。
         sess.run(train, feed_dict={x: X[start:end], y_: Y[start:end]})
-        if i %1000 == 0:
+        if step %1000 == 0:
             # 每隔一段时间计算在所有数据上的交叉熵并输出。
             total_cross_entropy = sess.run(cross_entropy, feed_dict={x: X, y_: Y})
-            print("After %d training step(s), cross entropy on all data is %g" % (i, total_cross_entropy))
+            print("After %d training step(s), cross entropy on all data is %g" % (step, total_cross_entropy))
             '''
             After 0 training step(s), cross entropy on all data is 0.0674925
             After 1000 training step(s), cross entropy on all data is 0.0163385
@@ -86,4 +96,7 @@ with tf.Session() as sess:
     它使得这个神经网络能更好的你和提供的训练数据。
     '''
 
+    print(sess.run(y, feed_dict={x: [[3.76580315e-01, 9.73783538e-01],
+                                     [4.17022005e-01, 7.20324493e-01],
+                                     [6.04716101e-01, 8.28845808e-01]]}))
 
