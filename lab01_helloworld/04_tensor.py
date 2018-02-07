2ae655fb57b2b9e43a84fc2b3359430e00163931
fanrong33 <fanrong33@qq.com>
2018-02-07 23:54:45 +0800

    update

--- lab01_helloworld/04_tensor.py
+++ lab01_helloworld/04_tensor.py
@@ -4,13 +4,12 @@
 import tensorflow as tf
 
 # tf.constant 是一个计算，这个计算的结果为一个张量，保存在变量a中
-a = tf.constant([1., 2.], name='a')
+a = tf.constant([1., 2.], name='a') # 常量a为一个长度为2的一维数组
 b = tf.constant([2., 3.], name='b')
 
 result = tf.add(a, b, name='add')
-print result
+print(result)
 '''
-Out:
 Tensor("add:0", shape=(2,), dtype=float32)
 shape=(2,) 说明了张量result是一个一维数组，这个数组的长度为2
 维度是张量一个很重要的属性，围绕张量的维度TensorFlow给出了很多有用的运算
@@ -18,9 +17,11 @@ shape=(2,) 说明了张量result是一个一维数组，这个数组的长度为
 
 # 在卷积神经网络中，卷积层或池化层有可能改变张量的维度，
 # 通过result.get_shape() 函数来获取结果张量的维度信息可以免去人工计算的麻烦
-print result.get_shape() 
-print result.dtype
+print(result.get_shape())
+print(result.dtype)
 
 with tf.Session() as sess:
     print(sess.run(result))
-
+    '''
+    [ 3.  5.]
+    '''
--- lab02_start/01_forward_propagation.py
+++ lab02_start/01_forward_propagation.py
@@ -3,12 +3,18 @@
  
 import tensorflow as tf 
 
-# 声明w1、w2两个变量。这里还通过 seed 参数设定了随机种子，
-# 这样可以保证每次运行得到的结果是一样的。
+"""
+输入层   隐藏层    输出层
+x * w1 = a
+         a * w2 = y
+"""
+
+# 声明w1、w2两个变量。2 -> 3 -> 1
+# 通过seed 参数设定了随机种子, 这样可以保证每次运行得到的结果是一样的。
 w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
 w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
 
-# 暂时将输入的特征向量定义为一个常量。注意这里 x 是一个 1*2 的矩阵。
+# 暂时将输入的特征向量定义为一个常量。注意这里 x 是一个 1*2 的矩阵，。
 x = tf.constant([ [0.7, 0.9] ])
 
 # 定义神经网络前向传播的过程
@@ -16,27 +22,27 @@ a = tf.matmul(x, w1)
 y = tf.matmul(a, w2)
 
 
-sess = tf.Session()
-# sess.run(w1.initializer)
-# sess.run(w2.initializer)
+with tf.Session() as sess:
+    # 因为w1和w2都还没有运行初始化过程。下面的两行分别初始化了w1和w2两个变量
+    # sess.run(w1.initializer)
+    # sess.run(w2.initializer)
 
-init = tf.global_variables_initializer()
-sess.run(init)
+    init = tf.global_variables_initializer()
+    sess.run(init)
 
-print(sess.run(y))
-'''
-[[ 3.95757794]]
-'''
+    print(sess.run(y))
+    '''
+    [[ 3.95757794]]
+    '''
 
-print(sess.run(w1))
-'''
-[[-0.81131822  1.48459876  0.06532937]
- [-2.44270396  0.0992484   0.59122431]]
-'''
-print(sess.run(w2))
-'''
-[[-0.81131822]
- [ 1.48459876]
- [ 0.06532937]]
- '''
-sess.close()
+    print(sess.run(w1))
+    '''
+    [[-0.81131822  1.48459876  0.06532937]
+     [-2.44270396  0.0992484   0.59122431]]
+    '''
+    print(sess.run(w2))
+    '''
+    [[-0.81131822]
+     [ 1.48459876]
+     [ 0.06532937]]
+     '''
