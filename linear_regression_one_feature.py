08c34248cac4376be1cff8918b7f90f3f8e12785
fanrong33 <fanrong33@qq.com>
2018-02-25 22:12:16 +0800

    add linear regression

--- linear_regression_one_feature.py
+++ linear_regression_one_feature.py
@@ -1,11 +1,12 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
 # Model linear regression y = Wx + b
 x = tf.placeholder(tf.float32, [None, 1])
 W = tf.Variable(tf.zeros([1,1]))
 b = tf.Variable(tf.zeros([1]))
-y = tf.matmul(x,W) + b   # prediction house price
+product = tf.matmul(x,W)
+y = product + b
 y_ = tf.placeholder(tf.float32, [None, 1])
 
 # Cost function sum((y_-y)**2)
@@ -14,15 +15,14 @@ cost = tf.reduce_mean(tf.square(y_-y))
 # Training using Gradient Descent to minimize cost
 train_step = tf.train.GradientDescentOptimizer(0.0000001).minimize(cost)
 
-init = tf.initialize_all_variables()
 sess = tf.Session()
+init = tf.initialize_all_variables()
 sess.run(init)
 steps = 1000
 for i in range(steps):
   # Create fake data for y = W.x + b where W = 2, b = 0
   xs = np.array([[i]])
   ys = np.array([[2*i]])
-
   # Train
   feed = { x: xs, y_: ys }
   sess.run(train_step, feed_dict=feed)
@@ -33,3 +33,4 @@ for i in range(steps):
   print("cost: %f" % sess.run(cost, feed_dict=feed))
 
 # NOTE: W should be close to 2, and b should be close to 0
+
